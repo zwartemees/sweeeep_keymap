16db4b56597de13ea361be194122de75a4c8e4c3
zwartemees <zwartemees@gmail.com>
2026-02-07 22:10:48 +0100

    new keyboard layout

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 git clone https://github.com/sadekbaroudi/qmk_firmware.git
 rm -rf qmk_firmware/keyboards/fingerpunch/src
 rm -rf qmk_firmware/keyboards/fingerpunch/sweeeeep
-cp src qmk_firmware/keyboards/fingerpunch
-cp sweeeeep qmk_firmware/keyboards/fingerpunch
+cp -r src qmk_firmware/keyboards/fingerpunch
+cp -r sweeeeep qmk_firmware/keyboards/fingerpunch
 
 cd qmk_firmware
-nix-shell
-CC=arm-none-eabi-gcc make fingerpunch/sweeeeep:default CONVERT_TO=rp2040_ce RGBLIGHT_ENABLE=yes
+nix-shell --run "CC=arm-none-eabi-gcc make fingerpunch/sweeeeep:default CONVERT_TO=rp2040_ce RGBLIGHT_ENABLE=yes"
+
